4faf5ef113fddadac38d977d48c5a02185468aee
Md Alkama <120254460+mdalkama@users.noreply.github.com>
2024-05-30 01:12:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let hamburger = document.querySelector(".material-symbols-outlined");
+let cards = document.querySelectorAll(".card");
+        hamburger.onclick = function(){
+            let navMenu = document.querySelector(".nav_bar");
+            navMenu.classList.toggle("active");
+        }
+
+        const btn = document.querySelectorAll('.question_main');
+        btn.forEach(question_main => {
+
+                const arrow = question_main.querySelector('.icon');
+                const answr = question_main.querySelector('.ans');
+                question_main.onclick = function(){
+                arrow.classList.toggle('rotate');
+                answr.classList.toggle('hide');
+            }
+        })
+        cards.forEach( ()=>{
+            
+        })
